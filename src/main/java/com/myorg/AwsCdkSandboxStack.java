b7001d6610abd12ecb03f1c0b79f1225f676a37d
ludwig <ludwig.nachname@gmail.com>
2023-08-04 07:22:43 +0200

    Implemented minimal example

--- src/main/java/com/myorg/AwsCdkSandboxStack.java
+++ src/main/java/com/myorg/AwsCdkSandboxStack.java
@@ -3,8 +3,7 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.s3.Bucket;
 
 public class AwsCdkSandboxStack extends Stack {
     public AwsCdkSandboxStack(final Construct scope, final String id) {
@@ -15,10 +14,7 @@ public class AwsCdkSandboxStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkSandboxQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Bucket.Builder.create(this, "MyFirstBucket")
+            .versioned(true).build();
     }
 }
